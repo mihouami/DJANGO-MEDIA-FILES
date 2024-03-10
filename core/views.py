80152163c733060664e75f395ff55e479285b37b
Minou <am.mihoubi@gmail.com>
2024-03-10 16:38:32 +0100

    02 - add model, form, view, template using widget-tweaks

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .forms import DogForm
 
 def index(request):
-    context = {'title':'Index'}
+    context = {'title':'Index', 'form':DogForm()}
     return render(request, 'index.html', context)
